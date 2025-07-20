bcbef9daef7ba3894bc157dbe0294b76e149d2cd
genvex <genvexteam@gmail.com>
2025-07-20 11:28:39 +0000

    auto-commit for 661f339b-1b93-4ce2-b860-6d46135b9c2e

--- frontend/src/components/Homepage_UA.jsx
+++ frontend/src/components/Homepage_UA.jsx
@@ -124,7 +124,11 @@ const Homepage_UA = () => {
                 </div>
                 <div className="flex items-start space-x-3">
                   <CheckCircle className="h-6 w-6 text-blue-600 mt-1 flex-shrink-0" />
-                  <p className="text-gray-700">Підтримуюче командне середовище в Гамбурзі</p>
+                  <p className="text-gray-700">Підтримуюче командне середовище в Гамбурзі з міцною згуртованістю</p>
+                </div>
+                <div className="flex items-start space-x-3">
+                  <CheckCircle className="h-6 w-6 text-blue-600 mt-1 flex-shrink-0" />
+                  <p className="text-gray-700">Цілодобова підтримка диспетчера та прямий зв'язок з командою</p>
                 </div>
               </div>
               <Button 
