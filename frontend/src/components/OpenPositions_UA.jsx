7c0bc9c82d165ed6b939055dcfa20724d18b4047
genvex <genvexteam@gmail.com>
2025-07-28 21:35:08 +0000

    auto-commit for d38fea00-c58b-41ee-bc59-e23918ddf679

--- frontend/src/components/OpenPositions_UA.jsx
+++ frontend/src/components/OpenPositions_UA.jsx
@@ -66,11 +66,11 @@ const OpenPositions_UA = () => {
                   </div>
                   <div className="flex items-center">
                     <Euro className="h-4 w-4 mr-1 text-blue-600" />
-                    Від €15,50/година + €10 щодня
+                    Від €15,50/година
                   </div>
                   <div className="flex items-center">
                     <UtensilsCrossed className="h-4 w-4 mr-1 text-blue-600" />
-                    Харчові нетто
+                    Харчові нетто €10 щодня
                   </div>
                 </div>
               </div>
