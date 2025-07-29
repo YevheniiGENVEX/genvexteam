3acb589fffb99afe6cd754a3d2b55401861e8494
genvex <genvexteam@gmail.com>
2025-07-29 16:00:20 +0000

    auto-commit for c1eb3802-896b-4f52-ba2a-fb770f31f9cd

--- frontend/src/components/Homepage_UA.jsx
+++ frontend/src/components/Homepage_UA.jsx
@@ -12,7 +12,7 @@ const Homepage_UA = () => {
   };
 
   return (
-    <section id="home" className="bg-gradient-to-br from-blue-50 to-white">
+    <section id="home" className="bg-gradient-to-br from-blue-50 to-white dark:from-gray-900 dark:to-gray-800">
       {/* Hero Section */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 lg:py-24">
         <div className="text-center">
