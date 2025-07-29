e8f77bd9164f1b612dffadc6ad96bc5ae4723288
genvex <genvexteam@gmail.com>
2025-07-29 13:36:19 +0000

    auto-commit for c7d5ccfd-e4e0-4d8c-81e0-b2cddf0f5ff9

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -97,7 +97,7 @@ const Header = () => {
                 onClick={() => scrollToSection('life')}
                 className="text-gray-700 hover:text-blue-600 transition-colors duration-200 font-medium text-left"
               >
-                Leben bei GENVEX
+                Leben bei GENVEX Team
               </button>
               <button
                 onClick={() => scrollToSection('faq')}
